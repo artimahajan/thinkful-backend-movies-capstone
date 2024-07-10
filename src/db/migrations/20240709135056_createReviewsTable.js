8c990620374f369aa15bb1ae3cc45da14b54a54d
Arati <arti.mahajan86@gmail.com>
2024-07-09 19:29:37 -0700

    Delete package-lock.json

--- src/db/migrations/20240709135056_createReviewsTable.js
+++ src/db/migrations/20240709135056_createReviewsTable.js
@@ -5,27 +5,19 @@
 - `critic_id`: (Foreign Key) A reference ID to a particular critic.
 - `movie_id`: (Foreign Key) A reference ID to a particular movie.
 */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable("reviews", (table) => {
     table.increments("review_id").primary();
     table.text("content");
     table.integer("score");
-    table.integer("critic_id").unsigned().notNullable();
-    table
-        .foreign("critic_id")
-        .references("critic_id")
-        .inTable("critics")
-        .onDelete("cascade");
-    table.integer("movie_id").unsigned().notNullable();
-    table
-        .foreign("movie_id")
-        .references("movie_id")
-        .inTable("movies")
-        .onDelete("cascade");
     table.timestamps(true, true);
- });
+    table.integer("critic_id").unsigned().notNullable();
+    table.foreign("critic_id").references("critic_id").inTable("critics");
+    table.integer("movie_id").unsigned().notNullable();
+    table.foreign("movie_id").references("movie_id").inTable("movies");
+  });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable("reviews");
 };
